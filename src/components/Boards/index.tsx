32c421f25c86e2b6c899f2f3dc9fbc19b992e145
Efim Shchegolev <efimblashck@yandex.ru>
2025-04-16 20:02:26 +0300

    feat: add boards page

--- src/components/Boards/index.tsx
+++ src/components/Boards/index.tsx
@@ -1,7 +1,19 @@
 import s from './Boards.module.scss'
+import { Link } from 'react-router'
 
 const index = () => {
-  return <div className={s.boards}></div>
+  return (
+    <section className={s.boards}>
+      <ul className={s.boardsList}>
+        <li className={s.boardCard}>
+          <h2 className={s.projectTitle}>Название</h2>
+          <Link className={s.link} to={'/board/1'}>
+            Перейти к доске
+          </Link>
+        </li>
+      </ul>
+    </section>
+  )
 }
 
 export default index
